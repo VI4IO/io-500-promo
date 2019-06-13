3bb81bb4ef4744c53adbfc3e7845f89049ac573b
Julian M. Kunkel <juliankunkel@googlemail.com>
2019-06-13 14:22:37 +0100

    Some analysis.

--- analysis/analyze.R
+++ analysis/analyze.R
@@ -14,11 +14,27 @@ g = ggplot(d, aes(x=1:nrow(d), y=io500__score)) +
   scale_color_gradientn(colours = rainbow(3)) + scale_y_log10() +
   #xlim(c(0, 0.1)) +
   #ylim(c(0, 500000)) +
-  labs(x="Rank #", y="IO-500 Score", title="Performance per rank")
+  labs(x="Rank #", y="IO-500 Score", title="Performance")
 ggsave("all-Performanceperrank.png", g, width=figsize[1], height=figsize[2], units="cm")
 plot(g)
 
 d$perThread = d$io500__score / d$information__client_total_procs
+
+
+g = ggplot(d, aes(x=information__client_nodes, y=io500__score)) +
+  geom_point() +
+  labs(x="Client nodes", y="IO-500 Score", title="Score achieved relative to the nodes")
+ggsave("all-Performance-nodes-withNodeCounts.png", g, width=figsize[1], height=figsize[2], units="cm")
+plot(g)
+
+
+g = ggplot(d, aes(x=information__client_nodes, y=io500__score/information__client_nodes)) +
+  geom_point()+
+  labs(x="Client nodes", y="IO-500 Score", title="Contribution to the IO-500 score per node")
+ggsave("all-Performance-nodes-pernode.png", g, width=figsize[1], height=figsize[2], units="cm")
+plot(g)
+
+
 g = ggplot(d, aes(x=1:nrow(d), y=perThread)) +
   geom_point(aes(col=information__filesystem_type)) +
   labs(x="Rank #", y="IO-500 Score per Process", title="Performance per process")
@@ -28,7 +44,33 @@ plot(g)
 g = ggplot(d, aes(x=information__filesystem_type, y=perThread)) +
   geom_boxplot() +
   labs(x="Rank #", y="IO-500 Score per Process", title="File system performance per process")
-ggsave("all-performancePerProcess-fs.png", g, width=figsize[1], height=figsize[2], units="cm")
+ggsave("all-performancePerProcess-fs.png", g, width=figsize[1]*2, height=figsize[2], units="cm")
+plot(g)
+
+t = d[grep( "NVMe", d$information__ds_storage_type),]
+g = ggplot(t, aes(x=information__filesystem_type, y=perThread)) +
+  geom_boxplot() +
+  ylim(c(0, 2.5)) +
+  labs(x="Rank #", y="IO-500 Score per Process", title="File system performance per process (NVME)")
+ggsave("all-performancePerProcess-fs-nvme.png", g, width=figsize[1], height=figsize[2], units="cm")
+plot(g)
+
+
+t = d[grep( "HDD", d$information__ds_storage_type),]
+g = ggplot(t, aes(x=information__filesystem_type, y=perThread)) +
+  geom_boxplot() +
+  ylim(c(0, 2.5)) +
+  labs(x="Rank #", y="IO-500 Score per Process", title="File system performance per process (HDD)")
+ggsave("all-performancePerProcess-fs-hdd.png", g, width=figsize[1], height=figsize[2], units="cm")
+plot(g)
+
+
+t = d[grep( "SSD", d$information__ds_storage_type),]
+g = ggplot(t, aes(x=information__filesystem_type, y=perThread)) +
+  geom_boxplot() +
+  ylim(c(0, 2.5)) +
+  labs(x="Rank #", y="IO-500 Score per Process", title="File system performance per process (SSD)")
+ggsave("all-performancePerProcess-fs-ssd.png", g, width=figsize[1]*2, height=figsize[2], units="cm")
 plot(g)
 
 
@@ -67,9 +109,18 @@ plot(g)
 # information__ds_storage_type
 # ggsave(g, "x.png")
 
+scores = function(d){
+  bd = max(d$ior__easy_write) * max(d$ior__easy_read) * max(d$ior__hard_write) * max(d$ior__hard_read)
+  md = max(d$mdtest__easy_write) * max(d$mdtest__easy_stat) * max(d$mdtest__easy_delete) * max(d$mdtest__hard_write) * max(d$mdtest__hard_read) * max(d$mdtest__hard_stat) * max(d$mdtest__hard_delete) * max(d$find__easy)
+  print(sprintf("bw: %.2f md: %.2f score: %.2f", bd^(1/4), md^(1/8), (md*bd)^(1/12)))
+}
+
+scores(d)
 
 # 10 Node analysis
 t = d %>% filter(information__client_nodes==10)
+scores(t)
+
 g = ggplot(t, aes(x=information__filesystem_type, y=io500__score)) +
   geom_boxplot() +
   labs(x="File system", y="IO-500 Score", title="Score per file system")
@@ -81,7 +132,7 @@ g = ggplot(t, aes(x=1:nrow(t), y=io500__score)) +
   geom_point(aes(col=log10(io500__bw), size=log10(io500__md))) +
   geom_smooth(method="loess", se=F) +
   scale_color_gradientn(colours = rainbow(3)) + scale_y_log10() +
-  labs(x="Rank #", y="IO-500 Score", title="10 Node Challenge -- Performance per rank")
+  labs(x="Rank #", y="IO-500 Score", title="10 Node Challenge")
 ggsave("10N-Performanceperrank.png", g, width=figsize[1], height=figsize[2], units="cm")
 plot(g)
 
--- certificates/create-certificates.sh
+++ certificates/create-certificates.sh
@@ -7,3 +7,6 @@ date="June 2019"
 ./create-certificate.py "$list" "$date" "IO-500" io500.csv io500 "io500__score"
 ./create-certificate.py "$list" "$date" "IO-500 BW Score" full.csv bw "io500__bw"
 ./create-certificate.py "$list" "$date" "IO-500 MD Score" full.csv md "io500__md"
+
+./create-certificate.py "$list" "$date" "10 Node BW Score" io500-10-node.csv 10node-bw "io500__bw"
+./create-certificate.py "$list" "$date" "10 Node MD Score" io500-10-node.csv 10node-md "io500__md"
